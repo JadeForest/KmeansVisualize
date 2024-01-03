ac613e4ef3b555b631ab33833a01bcb5339bb89e
Z YS <zooywans@126.com>
2024-01-03 14:44:05 +0800

    add grid data

--- data.py
+++ data.py
@@ -1,7 +1,24 @@
-import random
+import random, json
 
-def Kmeans(K:int, data:list) -> 'tuple[list[int], list[list[int]]]':
-    assert isinstance(data, list), 'data is not a list.'
+
+def eculidean_dist(p1: list[int], p2: list[int]):
+    sum = (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
+    return pow(sum, 1 / 2)
+
+
+def manhattan_dist(p1: list[int], p2: list[int]):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+DIST_FUNCS = {"eculidean": eculidean_dist, "manhattan": manhattan_dist}
+
+
+def Kmeans(
+    K: int, data: list, dist_func: str = "eculidean"
+) -> tuple[list[int], list[list[int]]]:
+    assert isinstance(data, list), "data is not a list."
+    assert dist_func in DIST_FUNCS.keys(), f"{dist_func} not valid."
+    distance = DIST_FUNCS[dist_func]
 
     n_data = len(data)
     # Random assign centers
@@ -9,15 +26,15 @@ def Kmeans(K:int, data:list) -> 'tuple[list[int], list[list[int]]]':
 
     while True:
         n_changes = 0
-        labels = find_label(data, centers)
+        labels = find_label(data, centers, dist_func)
 
         for label in list(set(labels)):
-            group = [data[i] for i in range(n_data) if labels[i]==label]
+            group = [data[i] for i in range(n_data) if labels[i] == label]
             transposed = list(zip(*group))
-            new_center = [sum(t)/len(group) for t in transposed]
+            new_center = [sum(t) / len(group) for t in transposed]
             old_center = centers[label]
             # Update centers
-            if eculidean_dist(old_center,new_center) > 1:
+            if distance(old_center, new_center) > 1:
                 centers[label] = new_center
                 n_changes += 1
         # Generate
@@ -26,52 +43,72 @@ def Kmeans(K:int, data:list) -> 'tuple[list[int], list[list[int]]]':
         if n_changes == 0:
             break
 
-    # return labels, centers
 
-
-def eculidean_dist(p1:'list[int]', p2:'list[int]'):
-    sum = (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
-    return pow(sum, 1/2)
-
-
-def find_label(data:list, centers:list) -> 'list[int]':
+def find_label(data: list, centers: list, dist_func: str) -> list[int]:
+    distance = DIST_FUNCS[dist_func]
     labels = [0] * len(data)
 
-    for i in range(len(data)):
+    for i, datapoint in enumerate(data):
         dists = [0] * len(centers)
-        for j in range(len(centers)):
-            dists[j] = eculidean_dist(data[i],centers[j])
-        labels[i] = dists.index( min(dists) )
+        for j, center in enumerate(centers):
+            dists[j] = distance(datapoint, center)
+        labels[i] = dists.index(min(dists))
 
     return labels
 
 
-def get_sample_data() -> list:
-    with open('data.txt',encoding='utf=8') as f:
-        return eval(f.read())
+def get_sample_data() -> list[list[float]]:
+    with open("data.json", encoding="utf=8") as f:
+        loaded = json.load(f)
+        return loaded["data"]
 
-def generate_data() -> list:
-    '''
-        Generates random clustered data 2D.
-    '''
+
+def generate_random_data() -> list:
+    """
+    Generates random clustered data 2D.
+    """
+    NUM_POINTS = random.randint(55, 85)
+    NUM_CLUSTERS = random.randint(3, 4)
+    mean = [random.randint(100, 200) for _ in range(4)] + [
+        random.randint(200, 400) for _ in range(4)
+    ]
+    random.shuffle(mean)
+
     data = []
-    mu = [random.randint(100,200) for i in range(4)]\
-        + [random.randint(200,400) for i in range(4)]
-    random.shuffle(mu)
-    for k in range(random.randint(3,4)):
-        for p in range(random.randint(45,55)):
+    for k in range(NUM_CLUSTERS):
+        for _ in range(NUM_POINTS):
             data.append(
-                [random.gauss(mu[2*k],30),random.gauss(mu[2*k+1],30)]
+                [random.gauss(mean[2 * k], 40), random.gauss(mean[2 * k + 1], 50)]
             )
-
+    # print(f"{NUM_POINTS=}, {NUM_CLUSTERS=}")
     return data
 
 
-if __name__ == '__main__':
-    data = get_sample_data()
-    for labels, centers in Kmeans(3, data):
-        print(labels)
-        print()
-        print(centers)
-        print()
+def generate_grid_data() -> list:
+    """
+    Generates random rectangular (grid) data 2D.
+    """
+    NUM_RECTANGLES = 4
+    end_point_pairs = [
+        [[random.randint(3, 30), random.randint(3, 30)] for _ in range(2)]
+        for _ in range(NUM_RECTANGLES)
+    ]
+
+    data: list[tuple] = []
+    for vector in end_point_pairs:
+        rect_data: list[tuple] = []
+        for x in range(
+            min(vector[0][0], vector[1][0]), max(vector[0][0], vector[1][0]) + 1
+        ):
+            for y in range(
+                min(vector[0][1], vector[1][1]), max(vector[0][1], vector[1][1]) + 1
+            ):
+                rect_data.append((x * 15, y * 15))
+        data.extend(rect_data)
+    return list(set(data))
+
+
+if __name__ == "__main__":
+    data = generate_grid_data()
+    print(data)
     ...
--- main.py
+++ main.py
@@ -3,15 +3,29 @@ import threading
 from tkinter import *
 from tkinter import messagebox
 
-from data import get_sample_data, generate_data, Kmeans
+from data import get_sample_data, generate_random_data, generate_grid_data, Kmeans
 
 # Window size
 WWIDTH = 720
 WHEIGHT = 570
 # Colors
-COLOR_LIST = ['#5CE600', '#1700E6', '#0AFFFF', '#E68A00', '#00CFE6',
-              '#61A0FF', '#FFA724', '#F09000', '#D175A3', '#993366',
-              '#0066CC', '#E600CF', '#6600CC', '#CC00CC', '#800080']
+COLOR_LIST = [
+    "#5CE600",
+    "#1700E6",
+    "#0AFFFF",
+    "#E68A00",
+    "#00CFE6",
+    "#61A0FF",
+    "#FFA724",
+    "#F09000",
+    "#D175A3",
+    "#993366",
+    "#0066CC",
+    "#E600CF",
+    "#6600CC",
+    "#CC00CC",
+    "#800080",
+]
 MAX_K = len(COLOR_LIST)
 
 
@@ -19,6 +33,7 @@ class App(Tk):
     def __init__(self):
         super().__init__()
         self.data = get_sample_data()
+        self.use_manhattan = False
         self.window_init()
         self.widgets_init()
 
@@ -27,85 +42,103 @@ class App(Tk):
         self.draw_points()
 
     def to_random(self):
-        self.data = generate_data()
+        self.data = generate_random_data()
+        self.draw_points()
+
+    def to_grid(self):
+        self.data = generate_grid_data()
         self.draw_points()
 
     def window_init(self):
-        self.title('Clustering demo (K-means)')
+        self.title("Clustering demo (K-means)")
         window_width, window_height = WWIDTH, WHEIGHT
-        x = (self.winfo_screenwidth() - window_width)/2
-        y = (self.winfo_screenheight() - window_height)/2
-        self.geometry('%dx%d+%d+%d' % (window_width, window_height, x, y))
+        x = (self.winfo_screenwidth() - window_width) / 2
+        y = (self.winfo_screenheight() - window_height) / 2
+        self.geometry("%dx%d+%d+%d" % (window_width, window_height, x, y))
         self.resizable(0, 0)
-
 
     def widgets_init(self):
         # Menu
         self.menu = Menu(self)
         self.data_select = Menu(self.menu)
-        self.menu.add_cascade(label='Options', menu=self.data_select)
-        self.data_select.add_command(label='Use default data (from data.txt)', command=self.to_default)
-        self.data_select.add_command(label='Use random generated data', command=self.to_random)
+        self.menu.add_cascade(label="Options", menu=self.data_select)
+        self.data_select.add_command(
+            label="Use default data (from data.json)", command=self.to_default
+        )
+        self.data_select.add_command(
+            label="Use randomly generated data", command=self.to_random
+        )
+        self.data_select.add_command(label="Use random grid data", command=self.to_grid)
         self.data_select.add_separator()
-        self.data_select.add_command(label='Exit', command=self.quit)
+        self.data_select.add_command(label="Exit", command=self.quit)
         self.config(menu=self.menu)
 
         # Canvas
         self.fm1 = Frame(self)
-        self.fig = Canvas(self.fm1, width=500, height=500, background='white')
+        self.fig = Canvas(self.fm1, width=500, height=500, background="white")
         self.draw_points()
         self.fig.pack(side=LEFT)
 
-        self.legend = Canvas(self.fm1, width=150,
-                             height=80, background='white')
-        self.legend.create_rectangle(37-4, 40-4, 37+4, 40+4,
-                                     width=0, fill='red')
-        self.legend.create_text(75, 40, text='Centroids')
+        self.legend = Canvas(self.fm1, width=150, height=80, background="white")
+        self.legend.create_rectangle(
+            37 - 4, 40 - 4, 37 + 4, 40 + 4, width=0, fill="red"
+        )
+        self.legend.create_text(75, 40, text="Centroids")
         self.legend.pack(side=RIGHT)
         self.fm1.pack(side=TOP)
 
         # Tips
-        self.tip = Label(self, text=f'K must less than {MAX_K}, better be 2~5.')
+        self.tip = Label(self, text=f"K must less than {MAX_K}, better be 2~5.")
         self.tip.pack()
 
         # Input and Button
         self.fm2 = Frame(self)
-        self.tip2 = Label(self.fm2,text='K=')
+        self.tip2 = Label(self.fm2, text="K=")
         self.tip2.pack(side=LEFT)
         self.kstr = StringVar()
         self.ety = Entry(self.fm2, width=10, textvariable=self.kstr)
         self.ety.pack(side=LEFT)
-        self.btn = Button(self.fm2, text='Run', width=10,
-                          height=1, command=self.run_calc)
-        self.btn.pack(side=RIGHT)
+        self.btn = Button(
+            self.fm2, text="Run", width=10, height=1, command=self.run_calc
+        )
+        self.btn.pack(side=LEFT)
+        self.checkbtn = Checkbutton(
+            self.fm2,
+            text="Use Manhattan Distance",
+            height=2,
+            variable=self.use_manhattan,
+        )
+        self.checkbtn.pack(side=RIGHT)
         self.fm2.pack(side=BOTTOM)
-
 
     def run_calc(self):
         # Handle input
-        kget:str = self.kstr.get()
+        kget: str = self.kstr.get()
 
         try:
-            K = int(kget) if len(kget)>0 else 1
-            if K < 1: raise ValueError
+            K = int(kget) if len(kget) > 0 else 1
+            if K < 1:
+                raise ValueError
         except ValueError:
-            messagebox.showerror(title='Input Error', message='Positive integer expected for K.')
+            messagebox.showerror(
+                title="Input Error", message="Positive integer expected for K."
+            )
             return
-        
+
         if K > MAX_K:
             self.highlight_tip(True)
             return
         else:
             self.highlight_tip(False)
-            
+
         # Calculate and Draw
-        self.disable_btn(True)
         drawing = threading.Thread(target=self.calc_and_draw, args=(K,))
         drawing.start()
 
-
     def calc_and_draw(self, K):
-        for labels, centers in Kmeans(K, self.data):
+        self.disable_btn(True)
+        dist_func = "manhattan" if self.use_manhattan else "eculidean"
+        for labels, centers in Kmeans(K, self.data, dist_func):
             self.draw_points(labels)
             self.draw_centers(centers)
             time.sleep(0.5)
@@ -119,22 +152,25 @@ class App(Tk):
 
         for i in range(len(self.data)):
             x, y = self.data[i]
-            self.fig.create_oval(x-3, y-3, x+3, y+3, width=0,
-                                 fill=COLOR_LIST[labels[i]])
+            self.fig.create_oval(
+                x - 3, y - 3, x + 3, y + 3, width=0, fill=COLOR_LIST[labels[i]]
+            )
 
     def draw_centers(self, centers):
         for x, y in centers:
-            self.fig.create_rectangle(x-4, y-4, x+4, y+4, width=0, fill='red')
+            self.fig.create_rectangle(x - 4, y - 4, x + 4, y + 4, width=0, fill="red")
 
-    def highlight_tip(self, highlight:bool):
-        fg = 'red' if highlight else 'black'
+    def highlight_tip(self, highlight: bool):
+        fg = "red" if highlight else "black"
         self.tip.config(fg=fg)
-            
-    def disable_btn(self, disabled:bool):
-        state = 'disabled' if disabled else 'normal'
-        self.btn.config(state=state)
-        
 
-if __name__ == '__main__':
+    def disable_btn(self, disabled: bool):
+        state = "disabled" if disabled else "normal"
+        self.btn.config(state=state)
+        self.checkbtn.config(state=state)
+        self.menu.entryconfig("Options", state=state)
+
+
+if __name__ == "__main__":
     app = App()
     app.mainloop()
